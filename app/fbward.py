2979e001f08359c2d5dd3ef0c16a49bb0aa12fc4
Muzio <ax.muzio@gmail.com>
2014-05-02 18:55:30 -0300

    Creating basic server

---
+++ app/fbward.py
@@ -0,0 +1,52 @@
+import cgi
+import urllib
+import json
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+import webapp2
+
+
+# def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
+#     """Constructs a Datastore key for a Guestbook entity with guestbook_name."""
+#     return ndb.Key('Guestbook', guestbook_name)
+
+
+
+class Ward(ndb.Model):
+    userid = ndb.IntegerProperty(required=True)
+    postid = ndb.IntegerProperty(required=True)
+    date = ndb.DateTimeProperty(auto_now_add=True)
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(json.dumps({'Hello' : 'World'}))
+
+class SearchAPI(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'application/json'  
+        self.response.write('Search Api')
+class InsertAPI(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'application/json'  
+        self.response.write('Insert Api')
+class QueryAPI(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'application/json'  
+        self.response.write('Query Api')
+
+class CheckAPI(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'application/json'  
+        self.response.write('Check Api')
+
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/search', SearchAPI),
+    ('/insert', InsertAPI),
+    ('/check', CheckAPI), 
+    ('/query', QueryAPI)
+], debug=True)
