025e114e55484b909bff1688cd64953c355eef48
Muzio <ax.muzio@gmail.com>
2014-05-02 21:28:49 -0300

    Creating DeleteAPI and QueryAPI

--- app/fbward.py
+++ app/fbward.py
@@ -12,10 +12,9 @@ import webapp2
 #     return ndb.Key('Guestbook', guestbook_name)
 
 
-
 class Ward(ndb.Model):
-    userid = ndb.IntegerProperty(required=True)
-    postid = ndb.IntegerProperty(required=True)
+    userid = ndb.StringProperty(required=True)
+    postid = ndb.StringProperty(required=True)
     date = ndb.DateTimeProperty(auto_now_add=True)
 
 
@@ -28,20 +27,43 @@ class SearchAPI(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'application/json'  
         self.response.write('Search Api')
+
 class InsertAPI(webapp2.RequestHandler):
-    def get(self):
+    def post(self):
         self.response.headers['Content-Type'] = 'application/json'  
         self.response.write('Insert Api')
+        user_id = self.request.get('user_id')
+        post_id = self.request.get('post_id')
+        ward = Ward(userid= user_id, postid= post_id)
+        ward.put()
+        self.response.write("User inserted")
+        # self.redirect('/')
+
+    def get(self):
+        self.error(405)
+
+
 class QueryAPI(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'application/json'  
         self.response.write('Query Api')
 
 class DeleteAPI(webapp2.RequestHandler):
-    def get(self):
+    def post(self):
         self.response.headers['Content-Type'] = 'application/json'  
-        self.response.write('Delete Api')
+        self.response.write('Delete Api\n')
+        user_id = self.request.get('user_id')
+        post_id = self.request.get('post_id')
+        wards = ndb.gql("SELECT * FROM Ward WHERE userid = %(user_id)s and postid = %(post_id)s" %{ "user_id" : user_id,
+                                                                                                    "post_id" : post_id})
+        # To Do
+        for w in wards:
+            w.key.delete()
 
+        self.response.write("User deleted")
+        # self.redirect('/')
+    def get(self):
+        self.error(405)
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
